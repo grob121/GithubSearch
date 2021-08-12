d2d38e3f92ddf5f54bbe8de7676410c3ffc57b2f
Allan Pagdanganan <allan.pagdanganan@accenture.com>
2021-08-12 18:52:06 +0800

    Initial Commit

---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by Allan Pagdanganan on 8/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
